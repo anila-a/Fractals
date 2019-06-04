faf5b87045b75f9e906abcf8d008f749f9fd6bdc
anila-a <51361033+anila-a@users.noreply.github.com>
2019-06-04 17:50:39 +0200

    Create adminHome.php

---
+++ administrator_page/adminHome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Fractals</title>
+<link rel="stylesheet" type="text/css" href="adminStyle.css">
+<link rel="icon" href="1497557606_Snowflake.ico">
+
+</head>
+<body>
+
+<?php include("homeHeader.php"); ?>
+
+<div id="box">
+<br>
+
+	<font size="5" face="Berlin Sans FB">ADMIN PAGE</font>
+	<div class="hline"></div>
+	<font size="3"><p>Welcome to Administrator Page.</p></font>
+</div>
+
+<div id="footer"><font size="2" face="Times New Roman">Copyright © 2017</div>
+
+</body>
+</html>
